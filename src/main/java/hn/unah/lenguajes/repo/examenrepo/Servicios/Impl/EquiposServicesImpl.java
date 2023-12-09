e4a8f16f02810ade058e6b253f92fb914ec5d89d
ale2002solano <ale133valle@gmail.com>
2023-12-08 21:27:53 -0600

    segundo

--- src/main/java/hn/unah/lenguajes/repo/examenrepo/Servicios/Impl/EquiposServicesImpl.java
+++ src/main/java/hn/unah/lenguajes/repo/examenrepo/Servicios/Impl/EquiposServicesImpl.java
@@ -1,5 +1,40 @@
 package hn.unah.lenguajes.repo.examenrepo.Servicios.Impl;
 
-public class EquiposServicesImpl {
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import hn.unah.lenguajes.repo.examenrepo.Modelos.Equipos;
+import hn.unah.lenguajes.repo.examenrepo.Repositorios.EquiposRepositorio;
+import hn.unah.lenguajes.repo.examenrepo.Servicios.EquiposService;
+
+public class EquiposServicesImpl implements EquiposService{
+
+    @Autowired
+    private EquiposRepositorio eRepositorio;
+    
+
+    @Override
+    public Equipos crearEquipos(Equipos equipos) {
+        return this.eRepositorio.save(equipos);
+       
+    }
+
+    @Override
+    public Equipos buscarEquiposId(Integer codigoequipo) {
+        return this.eRepositorio.findById(codigoequipo).get();
+       
+    }
+
+    @Override
+    public Equipos eliminarEquiposId(Integer codigoequipo) {
+        Equipos equipoAEliminar = this.eRepositorio.findById(codigoequipo)
+        .orElseThrow(() -> new NoSuchElementException("No se encontró el equipo con el ID proporcionado: " + codigoequipo));
+    
+    this.eRepositorio.deleteById(codigoequipo);
+    
+    return equipoAEliminar;
+        
+    }
     
 }
